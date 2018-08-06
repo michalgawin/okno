53b169a9ddd225acf4a89f9bf7cb55e7a5a23b90
mlgn <michalgawin85@gmail.com>
2018-08-06 23:14:21 +0200

    increase version of docker images

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 
-docker build --force-rm --no-cache -t michalgawin/okno-db:0.1 -f db.dockerfile .
-docker build --force-rm --no-cache -t michalgawin/okno-auth:0.2 -f auth.dockerfile .
-docker build --force-rm --no-cache -t michalgawin/okno-svc:0.2 -f svc.dockerfile .
+rem docker build --force-rm --no-cache -t michalgawin/okno-db:0.1 -f db.dockerfile .
+docker build --force-rm --no-cache -t michalgawin/okno-auth:0.3 -f auth.dockerfile .
+docker build --force-rm --no-cache -t michalgawin/okno-svc:0.3 -f svc.dockerfile .
